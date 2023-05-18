859b8166ff94ac70ea4695417c07331edf52e26c
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-17 23:13:13 -0300

    feat: adding eslint lib on the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-  return'🖐 hello world! 🪐'
+  return '🖐 hello world! 🪐'
 })
 
-app.listen({
-  port: 3333,
-}).then(() =>{
-  console.log('🚀 HTTP server running on http://localhost:3333');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
