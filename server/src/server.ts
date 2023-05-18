63f256af7dd3edb2043280ca192728357bd3bf36
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-17 22:50:45 -0300

    feat: adding fastify lib on the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,13 @@
-const nome: string = 'Gabriel'
+import fastify from 'fastify';
 
-console.log(nome)
+const app = fastify()
+
+app.get('/hello', () => {
+  return'🖐 hello world! 🪐'
+})
+
+app.listen({
+  port: 3333,
+}).then(() =>{
+  console.log('🚀 HTTP server running on http://localhost:3333');
+})
