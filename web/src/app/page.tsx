cf55f20823b1d8e0fdbbd921e0b5801b2b2825e4
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-18 16:55:15 -0300

    refactor: cleaning up the project once again, and using tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from '@/components/Button'
-
 export default function Home() {
   return (
-    <h1>
-      Hello World!
-      
-      <Button title='Gab' />
-    </h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Your time capsule</h1>
+    </div>
   )
 }
--- web/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-interface ButtonProps {
-  title: string
-}
-
-export function Button(props: ButtonProps) {
-  return (
-    <h1>{props.title}</h1>
-  )
-}
