335b3509c88ac56485770cd26575f2561614165c
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-19 02:11:38 -0300

    chore: adding eslint and prettier libs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="bg-gray-950 h-20 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello World!</Text>
+      <Text className="text-gray-50 font-bold text-5xl">Westside</Text>
       <StatusBar style="light" translucent />
     </View>
   );
