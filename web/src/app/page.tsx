a1bc6f6fd9778e2bb60534ab2882dbc24cc7730c
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-21 23:47:26 -0300

    feat: new color pallet for tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Your time capsule</h1>
+      <h1 className="font-alt text-4xl font-bold">JamJuree</h1>
     </div>
   )
 }
