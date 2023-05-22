977122ffee0f902de94a43e7a933a19deb800149
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-05-22 00:25:05 -0300

    feat: adding new implementation for home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,20 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Your time capsule</h1>
-      <h1 className="font-alt text-4xl font-bold">JamJuree</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[340px] text-center leading-relaxed">
+            You have not registered any memories yet, start to{' '}
+            <a href="" className="underline">
+              create now
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
